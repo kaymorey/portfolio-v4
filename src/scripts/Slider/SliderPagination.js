3287dd34557c33a4696bd2d17d3ce70401784121
kaymorey <moreirakatia@neuf.fr>
2017-06-12 16:48:14 +0200

    Add easings pagination slider

--- src/scripts/Slider/SliderPagination.js
+++ src/scripts/Slider/SliderPagination.js
@@ -1,4 +1,4 @@
-import TweenLite from 'gsap'
+import {TweenLite, Power3} from 'gsap'
 import Emitter from '../Emitter'
 
 export default class SliderPagination {
@@ -32,7 +32,8 @@ export default class SliderPagination {
         this.underline.dataset.selected = this.links.indexOf(link)
         TweenLite.to(this.underline, 0.7, {
             left: link.offsetLeft,
-            width: link.offsetWidth
+            width: link.offsetWidth,
+            ease: Power3.easeOut
         })
     }
 
@@ -44,8 +45,9 @@ export default class SliderPagination {
 
         let referenceWidth = this.links[selectedIndex].offsetWidth
 
-        TweenLite.to(this.underline, 0.4, {
-            width: referenceWidth + 15 * diff
+        TweenLite.to(this.underline, 0.5, {
+            width: referenceWidth + 15 * diff,
+            ease: Power3.easeInOut
         })
     }
 
@@ -53,8 +55,9 @@ export default class SliderPagination {
         let selectedIndex = this.underline.dataset.selected
         let referenceWidth = this.links[selectedIndex].offsetWidth
 
-        TweenLite.to(this.underline, 0.6, {
-            width: referenceWidth
+        TweenLite.to(this.underline, 0.5, {
+            width: referenceWidth,
+            ease: Power3.easeOut
         })
     }
 
